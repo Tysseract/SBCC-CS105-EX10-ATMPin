0bdf1785ff2bd9b20898b931d8dc42b35f1b1e84
Tysseract <ttis3.14@gmail.com>
2018-03-26 17:25:48 -0700

    I had forgotten the header

--- src/edu/sbcc/cs105/AtmPin.java
+++ src/edu/sbcc/cs105/AtmPin.java
@@ -1,3 +1,13 @@
+/**
+ * CS 105 Theory & Practice I
+ * CRN: 62499
+ * Assignment: AtmPin
+ * 
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ * 
+ * @author Mattys C vanZeyl
+ */
 package edu.sbcc.cs105;
 
 public class AtmPin {
